a97fcb8fc063698000276fb8b7729f90db127e0d
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-04-08 13:21:50 +0200

    starter files and js logic for game

--- donjon.js
+++ donjon.js
@@ -1,14 +1,31 @@
+const TURN_INSTRC = document.getElementById("turn-instructions");
+const ATTACK_BTNS = document.getElementById('attack-btn');
+const DEFENSE_BTNS = document.getElementById("defense-btn");
+
+alert('VOUS AVEZ RENCONTRÉ LE DONJON MASTER!!!! Préparez - vous à vous battre!');
+
 let playerHealth = 100;
 let bossHealth = 150;
-let attack;
-let defense;
-let choiceTrue;
+
+let attack = true;
+let bossAttack = false;
 const BOSS_ATTAQUE = 6;
 let choiceTable = ['bois', 'fer', 'magique'];
 let attackTable = [2, 5, 10];
 let defenseTable = [1, 3, 5];
 
-alert('VOUS AVEZ RENCONTRÉ LE DONJON MASTER!!!! Préparez - vous à vous battre!')
+
+let htmlMsg = TURN_INSTRC.innerHTML = "choisir une attaque!!!";
+let attackDisplay = ATTACK_BTNS.style.display = '';
+
+ATTACK_BTNS.addEventListener('click', e => {
+    let btn = e.target;
+    console.log(btn.innerHTML);
+
+
+})
+
+
 
 
 
@@ -17,6 +34,3 @@ alert('VOUS AVEZ RENCONTRÉ LE DONJON MASTER!!!! Préparez - vous à vous battre
 
 //calc boss attack
 //
-
-
-}
