c64e07fd2715caa574b42f2cdc1b960a262b49d0
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-04-08 13:01:16 +0200

    starter files

---
+++ donjon.js
@@ -0,0 +1,22 @@
+let playerHealth = 100;
+let bossHealth = 150;
+let attack;
+let defense;
+let choiceTrue;
+const BOSS_ATTAQUE = 6;
+let choiceTable = ['bois', 'fer', 'magique'];
+let attackTable = [2, 5, 10];
+let defenseTable = [1, 3, 5];
+
+alert('VOUS AVEZ RENCONTRÉ LE DONJON MASTER!!!! Préparez - vous à vous battre!')
+
+
+
+//chose a defense
+
+
+//calc boss attack
+//
+
+
+}
